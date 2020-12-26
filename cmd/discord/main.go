56122f21aee5a90e98cfc96f1c500fdf48531bee
David Li <davidli012345@gmail.com>
2020-12-25 22:34:49 -0800

    adding discord messages

--- cmd/discord/main.go
+++ cmd/discord/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"github.com/dli-invest/finreddit/pkg/discord"
 	"github.com/dli-invest/finreddit/pkg/types"
+	"log"
 )
 
 func main() {
 	discordEmbed := []types.DiscordEmbed{{Title: "sample title", Url: "https://github.com/FriendlyUser/finfiber/blob/master/pkg/discord/discord.go", Description: "CI/CD"}}
 	discordPayload := types.DiscordWebhook{Content: "test content", Embeds: discordEmbed}
-	discord.SendWebhook(discordPayload)
+	_, err := discord.SendWebhook(discordPayload)
+	if err != nil {
+		log.Println("Failed to send request")
+	}
 }
